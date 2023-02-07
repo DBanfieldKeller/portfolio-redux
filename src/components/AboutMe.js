35c86daf079e35cf3287642104091e59a5a19445
Goat <dpajbk@gmail.com>
2023-02-07 14:10:30 -0500

    updated about me and current page with basics

---
+++ src/components/AboutMe.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function AboutMe() {
+    return(
+        <div className="container">
+            <h3>About Me </h3>
+            <img className="img-fluid" src="https://raw.githubusercontent.com/DBanfieldKeller/react-portfolio/main/src/assets/FT84fB3XEAId64O.jpg" alt="me"/>    
+            <p>I am a full stack web developer with a number of frontend and backend applications that are on display in the projects section of this website. I graduated from UC Berkeley in 2016 with a BA in Linguistics and Japanese. Since that time I have worked for and with many Japanese companies. I am fluent in Japanese and comfortable in both American and Japanese business cultures. </p>
+        </div>
+    )
+}
