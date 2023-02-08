7b6b87e1e164fbc99c2ac614c3f09caf716d9188
Goat <dpajbk@gmail.com>
2023-02-08 13:53:01 -0500

    hero file created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React , {useState} from "react";
 import Navbar from './components/Navigation';
 import Header from './components/Header';
-
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
+      <Hero />
+      
       <h1>Hello World!</h1>
     </div>
   );
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hero () {
+    return (
+        <div>
+            Insert Picture Here
+        </div>
+    )
+};
+
+export default Hero;
