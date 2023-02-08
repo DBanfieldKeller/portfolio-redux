a0674edac40ab3b792851afef8da80503aef3a64
Goat <dpajbk@gmail.com>
2023-02-08 13:01:55 -0500

    placeholder navbar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React , {useState} from "react";
+import Navbar from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Hello World!</h1>
     </div>
   );
---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+function Navbar() {
+    return(
+        <nav>
+            This is a navbar
+        </nav>
+    )
+}
+
+export default Navbar   
