47bbe95387fae0b05e9681f0620235d0e8c7e820
Goat <dpajbk@gmail.com>
2023-02-23 13:20:32 -0500

    updated navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,37 @@
-import React, {useState} from "react";
+import React from 'react';
 
-function Navbar() {
-    return(
-        <nav>
-            This is a navbar
-        </nav>
-    )
+function Navigation(props) {   
+
+
+    return (
+        <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid">
+
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <div className="nav-link" onClick={(e)=>{
+                  props.setPage('AboutMe');
+                }}>About Me</div>
+              </li>
+              <li className="nav-item">
+                <div className="nav-link" onClick={(e)=>{
+                  props.setPage('Projects')
+                }}>Projects</div>
+              </li>
+              <li className="nav-item">
+                <div className="nav-link" onClick={(e)=>{
+                  props.setPage('ContactMe')
+                }}>Contact Me</div>
+              </li>
+              <li className="nav-item">
+                <div className="nav-link" onClick={(e)=>{
+                  props.setPage('Resume')
+                }}>Resume</div>
+              </li>
+            </ul>
+          </div>
+      </nav>
+  );
 }
 
-export default Navbar   
+export default Navigation;
